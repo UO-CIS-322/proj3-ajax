17cb1f257a3107b4407d6c844f0702cbed502d55
Ramsey McGrath <Ramsey_McGrath@symantec.com>
2017-10-20 23:05:12 -0700

    Commiting my local branches all at once... whoops

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -15,6 +15,15 @@ import arrow
 #  javadoc comments.
 #
 
+# Putting globals at the top of the file so theyre easy to edit. These to be in a config but eh
+
+start_control_hour_shift = 1
+
+max_brevet_distance_multiple = 1.20
+
+brevet_max_times_shift = [(200, 10), (400, 20)]
+
+control_speedlimits = [(1000, 13.333, 26), (600, 11.428, 28), (400, 15, 30), (300, 15, 32), (200, 15, 32), (0, 15, 34)]
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -29,8 +38,20 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
-
+    #Stop and returns false if we're out of bounds
+    if int(control_dist_km) > brevet_dist_km * max_brevet_distance_multiple:
+        return False
+    
+    #set control dist to brev distance if within acceptable bounds of final control 
+    control_dist_km = getControlDist(control_dist_km, brevet_dist_km)
+    
+    #loop to calculate hour value and successively decrease control distance to change speed limits
+    base_hours = 0
+    for dist, minspeed, maxspeed in control_speedlimits:
+        base_hours = calcHoursVals(control_dist_km, dist, maxspeed, base_hours)
+  
+    #Our time formatting broke a little, had to change it back t ISO
+    return arrow.get(brevet_start_time).shift(hours=base_hours).isoformat()
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -45,4 +66,50 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    #Control 1
+    closetime = arrow.get(brevet_start_time)
+    if control_dist_km == 0:
+        return closetime.shift(hours=start_control_hour_shift).isoformat()
+    
+    #Get control dist
+    control_dist_km = getControlDist(control_dist_km, brevet_dist_km)
+    
+    #Set close time
+    closetime = getCloseTime(control_dist_km, closetime)
+ 
+    #loop to change hours multiple and speed limits 
+    converted_hours = convertHours(control_dist_km)
+    return closetime.shift(hours=converted_hours).isoformat()
+
+################
+#
+#Helper methods are the bottom so thier values are defined
+#
+###########
+
+#Helper method to convert some hour data
+def convertHours(control_dist_km):
+    hours = 0
+    for dist, minspeed, maxspeed in control_speedlimits:
+        hours = calcHoursVals(control_dist_km, dist, minspeed, hours)
+    return hours
+
+#Helper method to get our close time
+def getCloseTime(control_dist_km, closetime):
+    for brevdist, minshift in brevet_max_times_shift:
+        if int(control_dist_km) == brevdist:
+            closetime = closetime.shift(minutes=minshift)
+    return closetime
+
+#helper method to get or control distance
+def getControlDist(control_dist_km, brevet_dist_km):
+    if (int(control_dist_km) <= brevet_dist_km * max_brevet_distance_multiple and int(control_dist_km) > brevet_dist_km):
+        control_dist_km = brevet_dist_km
+    return control_dist_km
+
+#Calculate the new values with speed and max speed and dist
+def calcHoursVals(control_dist_km, dist, maxspeed, base_hours):
+    if control_dist_km > dist:
+        base_hours += (control_dist_km - dist) / maxspeed
+        control_dist_km = dist
+    return base_hours
